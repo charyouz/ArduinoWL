b1275c225d920e723fb97c69f2da8e0a37e16aeb
charyouz <31696876+charyouz@users.noreply.github.com>
2017-09-06 19:01:17 +0300

    Add files via upload

---
+++ AWL_1.ino
@@ -0,0 +1,17 @@
+#include <SFE_BMP180.h>   //paine ja lämpötila anturin lib
+#include <Wire.h>         //sama
+
+SFE_BMP180 pressure;      //
+
+#define ALTITUDE 37     //laita tähän nykyinen korkeus metreissä
+
+void setup() {
+ 
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
